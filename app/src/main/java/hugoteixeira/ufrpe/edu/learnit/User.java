f7e38af5d53c3b0c9fc71af125e97ee2095c5bfb
Hugo Teixeira <hugosteixeira@hotmail.com>
2016-10-12 14:51:38 -0300

    User initial

--- app/src/main/java/hugoteixeira/ufrpe/edu/learnit/User.java
+++ app/src/main/java/hugoteixeira/ufrpe/edu/learnit/User.java
@@ -1,4 +1,14 @@
 package hugoteixeira.ufrpe.edu.learnit;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
 public class User {
+    private int id;
+    private String login;
+    private String password;
+    private String email;
+    private BigDecimal coins;
+    private ArrayList<String> intress;
+
 }
