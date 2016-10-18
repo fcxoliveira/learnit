644aae417bf334bb7a47c0302a3c38f451a29016
Filipe <filcarx@gmail.com>
2016-10-18 16:28:47 -0200

    User

--- app/src/main/java/ufrpe/edu/learnit/User.java
+++ app/src/main/java/ufrpe/edu/learnit/User.java
@@ -1,4 +1,33 @@
 package ufrpe.edu.learnit;
 
 public class User {
+
+    private String login;
+    private String senha;
+    private String email;
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 }
