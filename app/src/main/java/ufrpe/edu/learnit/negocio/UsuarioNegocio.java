87a0f50ca4a4f5dc3a4ce0146a9f644f76b4d5bc
Hugo Teixeira <hugosteixeira@hotmail.com>
2016-10-20 20:03:36 -0300

    Teste2

---
+++ app/src/main/java/ufrpe/edu/learnit/negocio/UsuarioNegocio.java
@@ -0,0 +1,24 @@
+package ufrpe.edu.learnit.negocio;
+
+import android.content.Context;
+
+import ufrpe.edu.learnit.dominio.Usuario;
+import ufrpe.edu.learnit.persistencia.UsuarioPersistencia;
+
+/**
+ * Created by Filipe on 20/10/2016.
+ */
+
+public class UsuarioNegocio {
+    private Context context;
+    public UsuarioNegocio(Context context){
+
+    }
+    public Usuario pesquisarUsuario(String login, String senha){
+        UsuarioPersistencia adapter = new UsuarioPersistencia(context);
+        adapter.open();
+        Usuario usuario = adapter.retornarUsuario(login,senha);
+        adapter.close();
+        return usuario;
+    }
+}
