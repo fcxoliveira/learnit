88026988066bd0bef2ffd41a3c921ac36053791c
Hugo Teixeira <hugosteixeira@hotmail.com>
2016-10-26 17:40:05 -0300

    Table Creates

--- app/src/main/java/ufrpe/edu/learnit/infra/persistencia/SessionPersistencia.java
+++ app/src/main/java/ufrpe/edu/learnit/infra/persistencia/SessionPersistencia.java
@@ -18,6 +18,7 @@ public class SessionPersistencia {
     public SessionPersistencia(){
         context = Session.getContext();
         dbHelper = new DataBaseHelper(context, DATABASE_NAME, null, DATABASE_VERSION);
+        dbHelper.onCreate(db);
     }
 
     public int idLogado(){
