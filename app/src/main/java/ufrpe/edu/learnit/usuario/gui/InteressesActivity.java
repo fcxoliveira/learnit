eb66e4391ab0be9e62de4bba1e06f8bb064cb6b5
joao_victor <joaovictorf3@gmail.com>
2016-10-27 16:55:18 -0200

    verificações nas tela de interesses

--- app/src/main/java/ufrpe/edu/learnit/usuario/gui/InteressesActivity.java
+++ app/src/main/java/ufrpe/edu/learnit/usuario/gui/InteressesActivity.java
@@ -3,20 +3,20 @@ package ufrpe.edu.learnit.usuario.gui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
 
 import ufrpe.edu.learnit.R;
 import ufrpe.edu.learnit.infra.dominio.Session;
 import ufrpe.edu.learnit.perfil.negocio.PerfilNegocio;
 
 public class InteressesActivity extends AppCompatActivity {
-    private EditText nome;
-    private EditText bio;
-    private Spinner interesse1,interesse2;
+    private EditText editTextNome;
+    private EditText editTextBio;
+    private Spinner interesse1, interesse2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,17 +29,35 @@ public class InteressesActivity extends AppCompatActivity {
         interesse2 = (Spinner) findViewById(R.id.spinner2);
         interesse2.setAdapter(adapter);
         Session.setContext(getApplicationContext());
-        nome = (EditText)findViewById(R.id.editText5);
-        bio = (EditText)findViewById(R.id.editText4);
+        editTextNome = (EditText) findViewById(R.id.editText5);
+        editTextBio = (EditText) findViewById(R.id.editText4);
     }
 
     public void chamarHome(View view) {
         Intent secondActivity = new Intent(this, HomeActivity.class);
         startActivity(secondActivity);
-        PerfilNegocio perfilNegocio = new PerfilNegocio();
-        perfilNegocio.cadastrarPerfil(Session.getUsuario().getID(),bio.getText().toString(),nome.getText().toString(),interesse1.getSelectedItem().toString(),interesse2.getSelectedItem().toString());
         this.finish();
     }
 
+    public boolean verificarNome(String nome) {
+        boolean autorizacao = false;
+        if (TextUtils.isEmpty(nome)) {
+            editTextNome.requestFocus();
+            editTextNome.setError("nome é um campo necessario");
+        } else {
+            autorizacao = true;
+        }
+        return autorizacao;
+    }
 
+    public void confirmar(View v){
+        if(verificarNome(editTextNome.getText().toString())){
+            PerfilNegocio perfilNegocio = new PerfilNegocio();
+            perfilNegocio.cadastrarPerfil(Session.getUsuario().getID(), editTextBio.getText().toString(), editTextNome.getText().toString(), interesse1.getSelectedItem().toString(), interesse2.getSelectedItem().toString());
+            chamarHome(v);
+        }else{
+            editTextNome.requestFocus();
+            editTextNome.setError("campo obrigatorio");
+        }
+    }
 }
