5023be23e4e215c24b8a7e57e65c776a506fe42a
Felipe <felipe.oficial.bsi@gmail.com>
2016-10-26 22:49:35 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/layout/activity_home.xml
    #	app/src/main/res/layout/activity_register.xml

--- app/src/main/java/ufrpe/edu/learnit/usuario/gui/InteressesActivity.java
+++ app/src/main/java/ufrpe/edu/learnit/usuario/gui/InteressesActivity.java
@@ -1,5 +1,6 @@
 package ufrpe.edu.learnit.usuario.gui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -19,12 +20,19 @@ public class InteressesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interesses);
-        String[] interesses = new String[] {"Informatica", "Musica", "Portugues", "Matematica", "Biologia", "Fisica", "Quimica", "Ed. Fisica"};
+        String[] interesses = new String[]{"Informatica", "Musica", "Portugues", "Matematica", "Biologia", "Fisica", "Quimica", "Ed. Fisica"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, interesses);
-        Spinner interesse1 = (Spinner)findViewById(R.id.spinner);
+        Spinner interesse1 = (Spinner) findViewById(R.id.spinner);
         interesse1.setAdapter(adapter);
-        Spinner interesse2 = (Spinner)findViewById(R.id.spinner2);
+        Spinner interesse2 = (Spinner) findViewById(R.id.spinner2);
         interesse2.setAdapter(adapter);
         Session.setContext(getApplicationContext());
     }
+
+    public void chamarHome(View view) {
+        Intent secondActivity = new Intent(this, HomeActivity.class);
+        startActivity(secondActivity);
+    }
+
+
 }
