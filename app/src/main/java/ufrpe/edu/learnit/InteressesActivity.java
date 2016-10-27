233918d6c9de147ee28208a9296576a9b571ce79
Filipe <filcarx@gmail.com>
2016-10-26 21:33:54 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/ufrpe/edu/learnit/LoginDataBaseAdapter.java
    #	app/src/main/java/ufrpe/edu/learnit/gui/LoginActivity.java
    #	app/src/main/res/layout/activity_register.xml

---
+++ app/src/main/java/ufrpe/edu/learnit/InteressesActivity.java
@@ -0,0 +1,29 @@
+package ufrpe.edu.learnit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import ufrpe.edu.learnit.infra.dominio.Session;
+
+/**
+ * Created by matheus on 26/10/16.
+ */
+
+public class InteressesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interesses);
+        String[] interesses = new String[] {"Informatica", "Musica", "Portugues", "Matematica", "Biologia", "Fisica", "Quimica", "Ed. Fisica"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, interesses);
+        Spinner interesse1 = (Spinner)findViewById(R.id.spinner);
+        interesse1.setAdapter(adapter);
+        Spinner interesse2 = (Spinner)findViewById(R.id.spinner2);
+        interesse2.setAdapter(adapter);
+        Session.setContext(getApplicationContext());
+    }
+}
--- app/src/main/java/ufrpe/edu/learnit/usuario/gui/HomeActivity.java
+++ app/src/main/java/ufrpe/edu/learnit/usuario/gui/HomeActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import ufrpe.edu.learnit.R;
