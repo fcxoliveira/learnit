9a20096174df05045b869f1acf405df8f8024eae
Hugo Teixeira <hugosteixeira@hotmail.com>
2016-10-27 21:06:48 -0300

    DBHelper

--- app/src/main/java/ufrpe/edu/learnit/infra/DataBaseHelper.java
+++ app/src/main/java/ufrpe/edu/learnit/infra/DataBaseHelper.java
@@ -4,20 +4,20 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
-
-import ufrpe.edu.learnit.usuario.persistencia.UsuarioPersistencia;
-
-
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
-    static final String DATABASE_NAME = "learnit.db";
-    static final int DATABASE_VERSION = 2;
+    private static final String DATABASE_NAME = "learnit.db";
+    private static final int DATABASE_VERSION = 5;
     private static final String TABLE_USER_CREATE = "create table IF NOT EXISTS USER (Id integer primary key autoincrement, Username  text,Password text, Email text);";
     private static final String TABLE_SESSION = "create table IF NOT EXISTS SESSION (LogedUserId integer);";
     private static final String TABLE_PERFIL = "create table IF NOT EXISTS PERFIL (IdPerfil int, Bio text, Nome text, Moedas "+
     "integer, Interesse1 text, Interesse2 text, Avaliacao real, Avaliadores integer, Horas integer);";
     private static final String TABLE_TAGS = "create table IF NOT EXISTS TAGS (Id integer primary key autoincrement, Tag text);";
     private static final String TABLE_AULAS = "create table if not exists AULAS(Id integer primary key autoincrement, Titulo text, Descricao text, Valor int, Tag1 int, Tag2 int,IdPerfil int, Horas int);";
+    private static final ArrayList<String> TAGS = new ArrayList<String>(Arrays.asList("Informatica", "Musica", "Portugues", "Matematica", "Biologia", "Fisica", "Quimica", "Ed. Fisica"));
+
 
     public DataBaseHelper(Context context , SQLiteDatabase.CursorFactory factory)
     {
@@ -31,21 +31,22 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         db.execSQL(TABLE_PERFIL);
         db.execSQL(TABLE_TAGS);
         db.execSQL(TABLE_AULAS);
+        for(String tag:TAGS) {
+            populateTags(tag,db);
+        }
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
     {
-        // Log the version upgrade.
         Log.w("TaskDBAdapter", "Upgrading from version " +oldVersion + " to " +newVersion + ", which will destroy all old data");
-
-        // Upgrade the existing database to conform to the new version. Multiple
-        // previous versions can be handled by comparing _oldVersion and _newVersion
-        // values.
-        // The simplest case is to drop the old table and create a new one.
         db.execSQL("DROP TABLE IF EXISTS " + "TEMPLATE");
-        // Create a new one.
         onCreate(db);
+    }
+
+    public void populateTags(String tag,SQLiteDatabase db){
+        String PopulateTags = "INSERT INTO TAGS (Tag) values ('"+ tag +"');";
+        db.execSQL(PopulateTags);
     }
 
 }
