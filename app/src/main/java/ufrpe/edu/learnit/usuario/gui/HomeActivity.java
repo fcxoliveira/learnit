f0548538018778d2f6f4b38d93c0ab484573a89a
Hugo Teixeira <hugosteixeira@hotmail.com>
2016-12-01 19:39:47 -0300

    I MADE THIS SHIT RUNS

--- app/src/main/java/ufrpe/edu/learnit/usuario/gui/HomeActivity.java
+++ app/src/main/java/ufrpe/edu/learnit/usuario/gui/HomeActivity.java
@@ -2,20 +2,21 @@ package ufrpe.edu.learnit.usuario.gui;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -29,27 +30,29 @@ import ufrpe.edu.learnit.infra.dominio.Session;
 import ufrpe.edu.learnit.infra.negocio.SessionNegocio;
 import ufrpe.edu.learnit.perfil.gui.PerfilActivity;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
     ListView listView;
     EditText editText;
     CustomAdapter adapter;
-
-
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Session.setContext(getApplicationContext());
-        listView = (ListView)findViewById(R.id.listView);
-        editText = (EditText)findViewById(R.id.editText6);
+        listView = (ListView) findViewById(R.id.listView);
+        editText = (EditText) findViewById(R.id.editText6);
         ArrayList<Aula> values = getValoresListView();
-        adapter= new CustomAdapter(values,getApplicationContext());
+        adapter = new CustomAdapter(values, getApplicationContext());
         setOnItemClickListener();
         listView.setAdapter(adapter);
         setOnlistenerSearch();
-
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
+
 
     public void setOnItemClickListener() {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -66,16 +69,22 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("Tem certeza que quer sair do APP?");
-        builder.setCancelable(true);
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                HomeActivity.super.finish();
-            }
-    });
-        AlertDialog alert = builder.create();
-        alert.show();
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Tem certeza que quer sair do APP?");
+            builder.setCancelable(true);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    HomeActivity.super.finish();
+                }
+            });
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+
     }
 
     public void fazerLogoff(View v){
@@ -127,8 +136,55 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+
+
+
+        }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
 
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            Toast.makeText(this, "Minhas aulas selecionado",Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
 
 }
